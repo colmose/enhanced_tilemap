14c47bed6b7e16bedfe5b5d6694242b947885284
Edwin Corrigan <edwintcorrigan@gmail.com>
2019-08-06 15:56:40 +0100

    More early days

--- public/vislib/marker_types/marker_cluster.js
+++ public/vislib/marker_types/marker_cluster.js
@@ -1,7 +1,7 @@
 define(function (require) {
   return function MarkerClusteringFactory(Private) {
-    let _ = require('lodash');
-    let L = require('leaflet');
+    const _ = require('lodash');
+    const L = require('leaflet');
 
     let BaseMarker = Private(require('./base_marker'));
 
@@ -19,41 +19,44 @@ define(function (require) {
       console.log(map, geoJson, params);
       MarkerClustering.Super.apply(this, arguments);
 
-      const markers = L.markerClusterGroup({
-        chunkedLoading: true,
-        spiderfyOnMaxZoom: true,
-        showCoverageOnHover: true,
-        iconCreateFunction: function (cluster) {
-
-          //Grouping the cluster returned by the server, if
-          const markers = cluster.getAllChildMarkers();
-          let markerCount = 0;
-
-          markers.forEach(function (m) {
-            markerCount = markerCount + m.count;
-          });
-
-          return new L.DivIcon({ html: '<div class=" clustergroup0 leaflet-marker-icon marker-cluster' +
-          'marker-cluster-medium leaflet-zoom-animated leaflet-clickable" tabindex="0" style="margin-left: -20px; ' +
-          'margin-top: -20px; width: 40px; height: 40px; z-index: 233;"><div><span>' + markerCount + '</span></div></div>' });
-        }
-      });
-
       this._createMarkerGroup({
-        map: map
+        map: map,
+        geoJson: geoJson,
+        params: params
       });
-
     }
-
-
 
     MarkerClustering.prototype._createMarkerGroup = function (options) {
 
-      this._markerGroup = L.markerClusterGroup();
+      this._markerGroup = L.markerClusterGroup({
+        chunkedLoading: true,
+        showCoverageOnHover: true,
+        // iconCreateFunction: function (cluster) {
 
-      this._markerGroup.addLayer(L.marker((options.map)));
-      //    this._fixTooltips();
-      this._addToMap();
+        //   //Grouping the cluster returned by the server, if
+        //   const markers = cluster.getAllChildMarkers();
+        //   let markerCount = 0;
+
+        //   markers.forEach(function (m) {
+        //     markerCount = markerCount + m.count;
+        //   });
+
+        //   return new L.DivIcon({
+        //     html: '<div class=" clustergroup0 leaflet-marker-icon marker-cluster' +
+        //       'marker-cluster-medium leaflet-zoom-animated leaflet-clickable" tabindex="0" style="margin-left: -20px; ' +
+        //       'margin-top: -20px; width: 40px; height: 40px; z-index: 233;"><div><span>' + markerCount + '</span></div></div>'
+        //   });
+        // }
+      });
+
+      for (let i = 0; i < options.geoJson.length; i++) {
+        const a = options.geoJson[i];
+        const title = 'testing geohash';
+        const marker = L.marker(new L.LatLng(a.geometry.coordinates[0], a.geometry.coordinates[1]), { title: title });
+        marker.bindPopup(title);
+        this._markerGroup.addLayer(marker);
+      }
+      console.log(this._markerGroup);
     };
 
     // /**
